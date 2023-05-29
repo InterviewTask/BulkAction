646217476a10a9aa02b3af4c18a6a742c0b6e3fa
Ali Hakimi <ali.hakimi.njf@gmail.com>
2023-05-29 10:35:45 +0330

    fix:add calender for search

--- src/app/pages/crud/components/table/table.component.ts
+++ src/app/pages/crud/components/table/table.component.ts
@@ -8,6 +8,7 @@ import { SelectionModel } from '@angular/cdk/collections';
 import { UserService } from '../../services/user.service';
 import { UserData } from '../../models/user.model';
 import { BulkActionComponent } from '../bulk-action/bulk-action.component';
+import { FormControl } from '@angular/forms';
 
 
 @Component({
@@ -17,22 +18,23 @@ import { BulkActionComponent } from '../bulk-action/bulk-action.component';
 })
 
 
-export class TableComponent implements OnInit,AfterViewInit  {
+export class TableComponent implements OnInit, AfterViewInit {
 
-  displayedColumns: string[] = ['select','id', 'email', 'status', 'access','date','action'];
+  date = new FormControl();
+  displayedColumns: string[] = ['select', 'id', 'email', 'status', 'access', 'date', 'action'];
   dataSource!: MatTableDataSource<UserData>;
   selection = new SelectionModel<any>(true, []);
   @ViewChild(MatPaginator) paginator!: MatPaginator;
   @ViewChild(MatSort) sort!: MatSort;
 
   constructor(
-    private dialog : MatDialog,
+    private dialog: MatDialog,
     private userService: UserService
   ) {
   }
 
-  getUserList(){
-    this.userService.getUser().subscribe(res=>{
+  getUserList() {
+    this.userService.getUser().subscribe(res => {
       this.dataSource = new MatTableDataSource(res);
       this.dataSource.paginator = this.paginator;
       this.dataSource.sort = this.sort;
@@ -43,15 +45,25 @@ export class TableComponent implements OnInit,AfterViewInit  {
   ngAfterViewInit() {
     this.dataSource.paginator = this.paginator;
     this.dataSource.sort = this.sort;
+    this.handelChangeDate();
   }
 
   ngOnInit(): void {
     this.getUserList();
 
+
   }
 
-  applyFilter(event: Event) {
-    const filterValue = (event.target as HTMLInputElement).value;
+  handelChangeDate() {
+    this.date.valueChanges.subscribe(res => {
+      console.log(res);
+      this.applyFilter()
+
+    })
+  }
+
+  applyFilter(event?: Event) {
+    const filterValue = event ? (event?.target as HTMLInputElement).value : this.date.value + '';
     this.dataSource.filter = filterValue.trim().toLowerCase();
 
     if (this.dataSource.paginator) {
@@ -83,48 +95,48 @@ export class TableComponent implements OnInit,AfterViewInit  {
     return `${this.selection.isSelected(row) ? 'deselect' : 'select'} row ${row.position + 1}`;
   }
 
-  bulkAssgin(){
-    const config : MatDialogConfig ={
+  bulkAssgin() {
+    const config: MatDialogConfig = {
       panelClass: 'app-full-bleed-dialog',
-      width:'30%'
+      width: '30%'
     }
-    const ref = this.dialog.open(AddUserComponent,config)
+    const ref = this.dialog.open(AddUserComponent, config)
 
-    ref.afterClosed().subscribe(res=>{
+    ref.afterClosed().subscribe(res => {
       console.log(res);
       this.userService.createBulk(res)
       this.getUserList();
     })
   }
-  editItem(row:UserData){
-    const config : MatDialogConfig ={
+  editItem(row: UserData) {
+    const config: MatDialogConfig = {
       panelClass: 'app-full-bleed-dialog',
-      width:'30%',
+      width: '30%',
       data: row
     }
-    const ref = this.dialog.open(AddUserComponent,config)
+    const ref = this.dialog.open(AddUserComponent, config)
 
-    ref.afterClosed().subscribe(res=>{
-      if(res){
+    ref.afterClosed().subscribe(res => {
+      if (res) {
         this.userService.editUser(res)
         this.getUserList();
       }
     })
   }
 
-  bulkAction(){
-    if(this.selection.selected.length < 1){
+  bulkAction() {
+    if (this.selection.selected.length < 1) {
       return
     }
 
-    const config : MatDialogConfig ={
+    const config: MatDialogConfig = {
       panelClass: 'app-full-bleed-dialog',
-      width:'30%',
+      width: '30%',
       data: this.selection.selected
     }
-    const ref = this.dialog.open(BulkActionComponent,config)
+    const ref = this.dialog.open(BulkActionComponent, config)
 
-    ref.afterClosed().subscribe(res=>{
+    ref.afterClosed().subscribe(res => {
       this.getUserList();
       this.selection.clear();
 
